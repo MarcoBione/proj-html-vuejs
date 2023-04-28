12e6b42250870043dcd7fc093dfb4413c9dce1ea
MarcoBione <marcobione.work@gmail.com>
2023-04-28 16:54:11 +0200

    Fix rounded properties

--- src/components/MainFourthComponent.vue
+++ src/components/MainFourthComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-white">
-        <div class="container debug">
+        <div class="container">
             <div class="row">
                 <div class="col p-0" v-for="card, index in store.maxCoachData">
-                    <div class="card py-5 h-100">
+                    <div class="card py-5 h-100 rounded-0">
                         <div class="card-body text-center d-flex flex-column justify-content-between">
                             <h5 class="card-title _mydarktxt fw-semibold mb-5">{{ card.name }}</h5>
                             <p class="card-text _mytxt fw-bolder fs-1 mb-5">{{ card.number }}</p>
